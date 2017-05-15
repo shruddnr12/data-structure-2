9487e9d945b1ce328de7d4fb3a2c95efccb40448
bit-user <bit-user@bit>
2017-05-15 09:29:44 +0900

    basic recursive

---
+++ src/recursion/RecursionTest.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class RecursionTest {
+
+	public static void main(String[] args) {
+		int count = 0;
+		recursive(count);
+	}
+	public static void recursive(int num){
+		if(num >= 3){
+			return ;
+		}
+		num ++;
+		System.out.println("recursive() called :" + num );
+		recursive(num);
+	}
+}
